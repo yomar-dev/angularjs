e70e45b0a5259083a18e28d658a286228f09a5ef
Yomar Miranda <mirandayomar@gmail.com>
2018-09-09 00:29:13 -0500

    HTTP Request - ng-repeat.

--- exercise-04-http-request/js/app.js
+++ exercise-04-http-request/js/app.js
@@ -2,7 +2,13 @@
 
     var app = angular.module('examplesApp',[ ]);
     app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+        $scope.teachers = {};
 
+        $http.get('json/teachers.json')
+            .then(function(response){
+                $scope.teachers = response.data.teachers;
+                console.log('Response: ', response.data.teachers)
+            })
     }]);
 
 })();
